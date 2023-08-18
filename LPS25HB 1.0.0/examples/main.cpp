f7b152f77a60e4ade42384e4dd4be348a914885b
IamMichaelthankyou <youkey555555@yahoo.co.jp>
2023-08-18 13:40:24 +0900

    フォーマットを忘れていたのでしました

--- LPS25HB 1.0.0/examples/main.cpp
+++ LPS25HB 1.0.0/examples/main.cpp
@@ -1,37 +1,37 @@
 #include <Arduino.h>
-#include <SPICREATE.h>
 #include <LPS25HB.h>
+#include <SPICREATE.h>
 #define CS 13
 #define SCK 33
 #define MISO 25
 #define MOSI 26
 SPICREATE::SPICreate spi;
 LPS lps;
-int flag=0;
-void setup(){
-  Serial.begin(115200);
-  Serial.println("serial set done");
-  delay(100);
-  spi.begin(VSPI,SCK,MISO,MOSI);
-  Serial.println("spi set done");
-  delay(100);
-  lps.begin(&spi,CS,5000000);
-  Serial.println("lps set done");
+int flag = 0;
+void setup() {
+    Serial.begin(115200);
+    Serial.println("serial set done");
+    delay(100);
+    spi.begin(VSPI, SCK, MISO, MOSI);
+    Serial.println("spi set done");
+    delay(100);
+    lps.begin(&spi, CS, 5000000);
+    Serial.println("lps set done");
 }
-void loop(){
-  if(Serial.available()){
-    if(Serial.read()=='l'){
-      flag=1;
+void loop() {
+    if (Serial.available()) {
+        if (Serial.read() == 'l') {
+            flag = 1;
+        }
     }
-  }
-  if(flag>0){
-    uint8_t data[3] ={};
-    lps.Get(data);
-    for(int i=0;i<3;i++){
-      Serial.print(data[i]);
-      Serial.print(",\t");
+    if (flag > 0) {
+        uint8_t data[3] = {};
+        lps.Get(data);
+        for (int i = 0; i < 3; i++) {
+            Serial.print(data[i]);
+            Serial.print(",\t");
+        }
+        Serial.println(lps.Plessure);
     }
-    Serial.println(lps.Plessure);
-  }
-  delay(1000);
+    delay(1000);
 }
