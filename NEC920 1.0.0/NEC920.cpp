a02d995f9a3e11bf690ce2a1b9fae489b0b960d4
YutoGCN <61838168+YutoGCN@users.noreply.github.com>
2023-10-10 14:32:19 +0900

    update NEC920.cpp

--- NEC920 1.0.0/NEC920.cpp
+++ NEC920 1.0.0/NEC920.cpp
@@ -1,5 +1,7 @@
 #include "NEC920.hpp"
 
+/*-----------------パケット操作関係の関数-----------------*/
+
 /**
  * @brief パケット作成関数
  *
@@ -14,8 +16,8 @@
  */
 uint8_t NEC920::makepacket(uint8_t *packet, uint8_t msgID, uint8_t msgNo, uint8_t *dst, uint8_t *src, uint8_t *parameter, uint8_t parameterLength)
 {
-    packet[0] = HEADER_0;
-    packet[1] = HEADER_1;
+    packet[0] = NEC920CONSTS::HEADER_0;
+    packet[1] = NEC920CONSTS::HEADER_1;
     packet[2] = parameterLength + 13;
     packet[3] = msgID;
     packet[4] = msgNo;
@@ -35,77 +37,129 @@ uint8_t NEC920::makepacket(uint8_t *packet, uint8_t msgID, uint8_t msgNo, uint8_
 }
 
 /**
- * @brief 送信先デバイスID設定関数
+ * @brief 受信データのメッセージIDを取得する関数
  *
- * @param id 4byte 送信先デバイスID
+ * @return int メッセージID
  */
-void NEC920::setDstID(uint8_t *id)
+uint8_t NEC920::getMsgID(uint8_t *arr)
 {
-    for (int i = 0; i < 4; i++)
-    {
-        dstID[i] = id[i];
-    }
+    return arr[3];
 }
+
+/**
+ * @brief 受信データのメッセージ識別番号を取得する関数
+ *
+ * @return int メッセージ識別番号
+ */
+uint8_t NEC920::getMsgNo(uint8_t *arr)
+{
+    return arr[4];
+}
+
+/**
+ * @brief 受信データを取得する関数
+ *
+ * @param arr 受信データを格納する配列
+ * @return int 受信データの長さ
+ */
+uint8_t NEC920::getMsgParam(uint8_t *arr)
+{
+    if (isMsgRecieved == 0)
+    {
+        return 0;
+    }
+    for (int i = 0; i < rxbff[2]; i++)
+    {
+        arr[i] = rxbff[i];
+    }
+    isMsgRecieved = 0;
+
+    return rxbff[2];
+}
+
+/*-----------------端子インターフェースの関数-----------------*/
+
+/**
+ * @brief 端子インターフェース設定関数
+ *
+ * @param pin920Reset // output L...リセット開始
+ * @param pin920Wakeup // output L...省電力移行 H...通常動作 (初期状態は通常動作)
+ * @param pin920Mode // input L...受信状態 H...省電力状態
+ */
+void NEC920::setPin(uint8_t pin920Reset, uint8_t pin920Wakeup, uint8_t pin920Mode)
+{
+    this->pin920Reset = pin920Reset;
+    this->pin920Wakeup = pin920Wakeup;
+    this->pin920Mode = pin920Mode;
+
+    pinMode(pin920Reset, OUTPUT);
+    pinMode(pin920Wakeup, OUTPUT);
+    digitalWrite(pin920Reset, HIGH);
+    digitalWrite(pin920Wakeup, HIGH);
+    pinMode(pin920Mode, INPUT);
+}
+
+/**
+ * @brief 省電力状態に移行する関数
+ *
+ */
+void NEC920::goSleep()
+{
+    digitalWrite(pin920Wakeup, LOW);
+}
+
+/**
+ * @brief 通常動作状態に移行する関数
+ *
+ */
+void NEC920::goWakeUp()
+{
+    digitalWrite(pin920Wakeup, HIGH);
+}
+
+/*-----------------シリアルポート関係の関数-----------------*/
 
 /**
  * @brief シリアルポート設定関数
  *
  * @param serial シリアルポート
+ * @param baudrate ボーレート
+ * @param rx 受信ピン
+ * @param tx 送信ピン
  */
-void NEC920::setSerial(HardwareSerial *serial)
+void NEC920::beginSerial(HardwareSerial *serial, uint32_t baudrate, uint8_t rx, uint8_t tx)
 {
-    this->ser = serial;
+    ser = serial;
+    ser->begin(baudrate, SERIAL_8N1, rx, tx);
 }
 
 /**
- * @brief
+ * @brief シリアルポートの有効性を確認する関数
  *
- * @param packet
- * @return int
+ * @return uint8_t 0...有効 1...無効
  */
-int NEC920::send(uint8_t *packet)
+uint8_t NEC920::isSerialValid()
 {
-    if (isSendingLocked == 1)
+    if (ser == NULL)
     {
         return 1;
     }
-    isSendingLocked = 1;
-    msgNoSendingMSG = packet[4];
-    ser->write(packet, packet[2]);
-    timeSendingMSG = micros();
-
     return 0;
 }
 
-/**
- * @brief データ送信関数
- * 送信先デバイスIDはsetDstIDで設定されたIDになる．
- * @param arr 送信するデータ
- * @param length 送信するデータの長さ
- * @return int 0...正常終了 1...送信ロック中 2...シリアルポートが設定されていない
- */
-int NEC920::sendData(uint8_t msgId, uint8_t msgNo, uint8_t *parameter, uint8_t parameterLength)
-{
-    uint8_t packet[PACKET_MAX_LENGTH];
-
-    makepacket(packet, msgId, msgNo, dstID, srcID, parameter, parameterLength);
-
-    send(packet);
-
-    return 0;
-}
+/*-----------------送受信コア-----------------*/
 
 /**
  * @brief 受信関数
  * 無線通信モジュールからの受信を行う関数，受信したデータはrxbffに格納される．
  * 受信したデータはisMsgRecievedで管理され，isMsgRecievedが1の時は受信済み．
- * @return int 0...正常終了 1...シリアルポートが設定されていない
+ * @return int 0...メッセージはない　1...メッセージがある
  */
-int NEC920::recieve()
+uint8_t NEC920::recieve()
 {
     if (ser == NULL)
     {
-        return 1;
+        return 0;
     }
 
     if (isMsgRecieved == 1)
@@ -117,17 +171,17 @@ int NEC920::recieve()
     {
         if (rxIndex == 0)
         {
-            if (ser->read() == HEADER_0)
+            if (ser->read() == NEC920CONSTS::HEADER_0)
             {
-                rxbff[rxIndex] = HEADER_0;
+                rxbff[rxIndex] = NEC920CONSTS::HEADER_0;
                 rxIndex++;
             }
         }
         else if (rxIndex == 1)
         {
-            if (ser->read() == HEADER_1)
+            if (ser->read() == NEC920CONSTS::HEADER_1)
             {
-                rxbff[rxIndex] = HEADER_1;
+                rxbff[rxIndex] = NEC920CONSTS::HEADER_1;
                 rxIndex++;
             }
             else
@@ -147,22 +201,7 @@ int NEC920::recieve()
             rxIndex = 0;
             isMsgRecieved = 1;
 
-            // 送信ロック解除(応答idが0x00または0x12でメッセージ識別番号が送信したメッセージ識別番号と一致する場合)
-            if ((getMsgID(rxbff) == 0x00 || getMsgID(rxbff) == 0x12) && getMsgNo(rxbff) == msgNoSendingMSG)
-            {
-                if (getMsgID(rxbff) == 0x00)
-                {
-                    ESP_LOGV("NEC920", "ACK(success):%02X", getMsgNo(rxbff));
-                }
-                else
-                {
-                    ESP_LOGV("NEC920", "ACK(fail):%02X", getMsgNo(rxbff));
-                }
-                isMsgRecieved = 0;
-                isSendingLocked = 0;
-            }
-
-            return 0;
+            return 1;
         }
         else
         {
@@ -174,51 +213,12 @@ int NEC920::recieve()
 }
 
 /**
- * @brief 受信データのメッセージIDを取得する関数
- *
- * @return int メッセージID
- */
-int NEC920::getMsgID(uint8_t *arr)
-{
-    return arr[3];
-}
-
-/**
- * @brief 受信データのメッセージ識別番号を取得する関数
- *
- * @return int メッセージ識別番号
- */
-int NEC920::getMsgNo(uint8_t *arr)
-{
-    return arr[4];
-}
-
-/**
- * @brief 受信データを取得する関数
- *
- * @param arr 受信データを格納する配列
- * @return int 受信データの長さ
- */
-int NEC920::getRecievedData(uint8_t *arr)
-{
-    if (isMsgRecieved == 0)
-    {
-        return 0;
-    }
-    for (int i = 0; i < rxbff[2]; i++)
-    {
-        arr[i] = rxbff[i];
-    }
-    isMsgRecieved = 0;
-
-    return rxbff[2];
-}
-
-/**
  * @brief 無線設定関数
  * 送信出力，チャンネル，RFバンド，CSモードを設定する．
+ *
+ * @return uint8_t 0...成功 1...失敗
  */
-void NEC920::setRfConf(uint8_t Power, uint8_t Channel, uint8_t RF_Band, uint8_t CS_Mode)
+uint8_t NEC920::setRfConf(uint8_t Power, uint8_t Channel, uint8_t RF_Band, uint8_t CS_Mode)
 {
     uint8_t parameter[4];
     parameter[0] = Power;
@@ -226,23 +226,23 @@ void NEC920::setRfConf(uint8_t Power, uint8_t Channel, uint8_t RF_Band, uint8_t 
     parameter[2] = RF_Band;
     parameter[3] = CS_Mode;
     uint8_t packet[17];
-    makepacket(packet, 0x21, 0x00, srcID, srcID, parameter, 4);
+    makepacket(packet, 0x21, 0x71, dummyID, dummyID, parameter, 4);
     ser->write(packet, 17);
-}
 
-/**
- * @brief 無線モジュールの生存確認関数
- *
- * @return int 0...生存 1...死亡
- */
-int NEC920::isWirelessModuleDead()
-{
-    if (isSendingLocked == 1)
+    while (recieve() == 0)
     {
-        if (micros() - timeSendingMSG > 1000000)
-        {
-            return 1;
-        }
-        }
-    return 0;
+        NOP();
+    }
+    uint8_t RfConfResult = getMsgID(rxbff);
+    ESP_LOGV("NEC920", "returnMsg ID:%02X No:%02X", RfConfResult, getMsgNo(rxbff));
+    if (RfConfResult == NEC920CONSTS::MSGID_RETURN_OK)
+    {
+        ESP_LOGV("NEC920", "RfConf Success");
+        return 0;
+    }
+    else
+    {
+        ESP_LOGV("NEC920", "RfConf Failed");
+        return 1;
+    }
 }
--- NEC920 1.0.0/NEC920.hpp
+++ NEC920 1.0.0/NEC920.hpp
@@ -6,48 +6,61 @@
 
 #include "Arduino.h"
 
-#define HEADER_0 0x0F
-#define HEADER_1 0x5A
-#define PACKET_MAX_LENGTH 254
+namespace NEC920CONSTS
+{
+    constexpr uint8_t HEADER_0 = 0x0F;
+    constexpr uint8_t HEADER_1 = 0x5A;
+    constexpr uint8_t PACKET_MAX_LENGTH = 254;
 
-#define SEND_CMD_ID 0x13
+    constexpr uint8_t MSGID_SEND = 0x11;
+    constexpr uint8_t MSGID_SEND_NORESEND = 0x13;
+    constexpr uint8_t MSGID_RETURN_OK = 0x00;
+    constexpr uint8_t MSGID_RETURN_NG = 0x01;
+}
 
 class NEC920
 {
 private:
-    /* data */
-    uint8_t dstID[4];
-    uint8_t srcID[4] = {0xFF, 0xFF, 0xFF, 0xFF};
+    uint8_t dummyID[4] = {0xFF, 0xFF, 0xFF, 0xFF};
 
     HardwareSerial *ser;
 
     /*送信をロックする用の変数*/
     bool isSendingLocked = 0;
-    uint8_t msgNoSendingMSG;
-    uint32_t timeSendingMSG;
+    uint8_t msgNoLastSendingMSG;
+    uint32_t timeSendingLastMSG;
 
     /*受信用変数*/
     uint8_t rxbff[256];
     uint8_t rxIndex = 0;
     bool isMsgRecieved = 0;
 
-public:
-    void setDstID(uint8_t *id);
+    /*ピン*/
+    uint8_t pin920Reset;  // output L...リセット開始
+    uint8_t pin920Wakeup; // output L...省電力移行 H...通常動作
+    uint8_t pin920Mode;   // input L...受信状態 H...省電力状態
+
+    /*-----------------パケット操作関係の関数-----------------*/
     uint8_t makepacket(uint8_t *packet, uint8_t msgID, uint8_t msgNo, uint8_t *dst, uint8_t *src, uint8_t *parameter, uint8_t parameterLength);
+    uint8_t getMsgID(uint8_t *arr);
+    uint8_t getMsgNo(uint8_t *arr);
+    uint8_t getMsgParam(uint8_t *arr);
 
-    void setSerial(HardwareSerial *serial);
+public:
+    /*-----------------端子インターフェースの関数-----------------*/
+    void setPin(uint8_t pin920Reset, uint8_t pin920Wakeup, uint8_t pin920Mode);
+    void goSleep();
+    void goWakeUp();
 
-    int send(uint8_t *packet);
-    int sendData(uint8_t msgId, uint8_t msgNo, uint8_t *parameter, uint8_t parameterLength);
-    int recieve();
-    int getRecievedData(uint8_t *arr);
+    /*-----------------シリアルポート関係の関数-----------------*/
+    void beginSerial(HardwareSerial *serial, uint32_t baudrate, uint8_t rx, uint8_t tx);
+    uint8_t isSerialValid();
 
-    int getMsgID(uint8_t *arr);
-    int getMsgNo(uint8_t *arr);
+    /*-----------------送受信コア-----------------*/
+    uint8_t recieve();
 
-    void setRfConf(uint8_t Power, uint8_t Channel, uint8_t RF_Band, uint8_t CS_Mode);
-
-    int isWirelessModuleDead();
+    /*-----------------各種コマンド-----------------*/
+    uint8_t setRfConf(uint8_t Power, uint8_t Channel, uint8_t RF_Band, uint8_t CS_Mode);
 };
 
 #endif
