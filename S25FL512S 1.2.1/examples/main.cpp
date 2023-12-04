b7390466ec64513df54302112588dc4f89fe51f2
23BYasu <414tobutaka@gmail.com>
2023-12-04 22:12:20 +0900

    S25FL512のライブラリ動作確認 3

--- S25FL512S 1.2.1/examples/main.cpp
+++ S25FL512S 1.2.1/examples/main.cpp
@@ -1,4 +1,6 @@
-// Copyright [2023] <CREATE-ROCKET>
+/*
+Copyright [2023] <CREATE TokyoTech>
+*/
 #include <Arduino.h>
 #include <S25FL512S.h>
 
@@ -13,7 +15,11 @@ uint8_t rx[256] = {};
 #define MOSI1 26
 
 SPICREATE::SPICreate SPIC;
-Flash flash;
+Flash flash1;
+
+const int PageSize = 0x100;
+
+// SPIflash S25FL512S用サンプルコード
 
 void setup() {
     // FlashのCSピンを非選択に設定
@@ -23,32 +29,36 @@ void setup() {
     // SPIの初期化
     SPIC.begin(VSPI, SCK1, MISO1, MOSI1);
     // Flashの初期化
-    flash.begin(&SPIC, flashCS, SPIFREQ);
+    flash1.begin(&SPIC, flashCS, SPIFREQ);
+    // Flashのデータを一括消去
+    flash1.erase();
 
-    Serial.println("Start");
-    flash.erase();
+    int count = 0;
 }
 
-bool check = false;
-
 void loop() {
-    if (!check) {
+    if (count < 3) {
         // 空いているところを探す
-        uint32_t Addr = flash.setFlashAddress();
+        uint32_t Addr = flash1.setFlashAddress();
         // 書き込むデータを作る
         for (int i = 0; i < 256; i++) {
             tx[i] = i;
         }
         // 書き込み
-        flash.write(Addr, tx);
-        delay(1);
+        flash1.write(Addr, tx);
+        delay(1000);
         // 読み込み
-        flash.read(Addr, rx);
+        flash1.read(Addr, rx);
         // 読み込んだデータをシリアルで表示
+        Serial.print("at page ");
+        Serial.println(count + 1);
+        delay(100);
         for (int i = 0; i < 256; i++) {
             Serial.print(rx[i]);
             Serial.print(" ");
         }
-        check = true;
+        count++;
+        Serial.println("");
+        delay(100);
     }
 }
