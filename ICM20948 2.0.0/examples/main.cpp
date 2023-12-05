f1584a1dd561262ac750e1a285a2a8d7b1044561
Katsuki096 <katsuki10632@gmail.com>
2023-12-05 17:52:38 +0900

    エラーの修正

--- ICM20948 2.0.0/examples/main.cpp
+++ ICM20948 2.0.0/examples/main.cpp
@@ -6,39 +6,38 @@ Copyright [2023] <CREATE TokyoTech>
 #include <ICM20948.h>
 #include <SPICREATE.h>
 
-#define ICMSCK 33 
-#define ICMMISO 25 
-#define ICMMOSI 26 
-#define ICMCS 27 
-#define SPIFREQ 120000
+#define ICMSCK 33   
+#define ICMMISO 25  
+#define ICMMOSI 26  
+#define ICMCS 27    
+#define SPIFREQ 1200000
 
 ICM icm20948;
 
 SPICREATE::SPICreate SPIC;
 
-
 void setup() {
-  Serial.begin(115200);
-  SPIC.begin(VSPI, ICMSCK, ICMMISO, ICMMOSI);
-  icm20948.begin(&SPIC, ICMCS, SPIFREQ);
-  uint8_t a;
-  a = icm20948.WhoAmI();
-  Serial.print(a);
+    Serial.begin(115200);
+    SPIC.begin(VSPI, ICMSCK, ICMMISO, ICMMOSI);
+    icm20948.begin(&SPIC, ICMCS, SPIFREQ);
+    uint8_t a;
+    a = icm20948.WhoAmI();
+    Serial.print(a);
 }
 
 void loop() {
-  int16_t ICM_data[6];
-  uint8_t ICM_data_raw[14];
-  icm20948.Get(ICM_data,ICM_data_raw);
-  Serial.print(ICM_data[0]);
-  Serial.print(",");
-  Serial.print(ICM_data[1]);
-  Serial.print(",");
-  Serial.print(ICM_data[2]);
-  Serial.print(",");
-  Serial.print(ICM_data[3]);
-  Serial.print(",");
-  Serial.print(ICM_data[4]);
-  Serial.print(",");
-  Serial.println(ICM_data[5]);
+    int16_t ICM_data[6];
+    uint8_t ICM_data_raw[14];
+    icm20948.Get(ICM_data, ICM_data_raw);
+    Serial.print(ICM_data[0]);
+    Serial.print(",");
+    Serial.print(ICM_data[1]);
+    Serial.print(",");
+    Serial.print(ICM_data[2]);
+    Serial.print(",");
+    Serial.print(ICM_data[3]);
+    Serial.print(",");
+    Serial.print(ICM_data[4]);
+    Serial.print(",");
+    Serial.println(ICM_data[5]);
 }
