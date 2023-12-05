da707f291b547584355f92160ab2ff3a9b0930f7
HosokawaYuki <hswk.yk1010@gmail.com>
2023-12-05 20:14:40 +0900

    Get2を追加し、その他数点修正しました。

--- H3LIS331  1.2.0/examples/main.cpp
+++ H3LIS331  1.2.0/examples/main.cpp
@@ -1,5 +1,8 @@
-// Copyright [2023] <CREATE-ROCKET>
+/*
+ Copyright [2023] <CREATE TokyoTech>
+*/
 // version: 1.2.0
+
 
 #include <Arduino.h>
 #include <H3LIS331.h>
@@ -18,6 +21,8 @@ H3LIS331 H3lis331;
 
 SPICREATE::SPICreate SPIC;
 
+int get = 1; //GetとGet2の切り替えのための変数
+
 void setup() {
     // put your setup code here, to run once:
     Serial.begin(115200);
@@ -26,30 +31,42 @@ void setup() {
     SPIC.begin(VSPI, H3LIS331SCK, H3LIS331MISO, H3LIS331MOSI);
     H3lis331.begin(&SPIC, H3LIS331CS, SPIFREQ);
 
-    // WhoAmI
     uint8_t a;
-    a = H3lis331.WhoAmI();
+    a = H3lis331.WhoAmI(); //whoamiを取得
     Serial.print("WhoAmI:");
-    Serial.println(a);
+    Serial.println(a); //取得したwhoamiを表示
     if (a == 0x32) {
         Serial.println("H3LIS331 is OK");
-        WhoAmI_Ok = true;
+        WhoAmI_Ok = true; 
     } else {
         Serial.println("H3LIS331 is NG");
     }
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
-    // if (WhoAmI_Ok == true) {
-    //     int16_t rx[3];
-    //     H3lis331.Get(rx);
-    //     Serial.print(rx[0]);
-    //     Serial.print(",");
-    //     Serial.print(rx[1]);
-    //     Serial.print(",");
-    //     Serial.println(rx[2]);
-    //     delay(10000);
+   // put your main code here, to run repeatedly:
+   if (WhoAmI_Ok == true) {
+    if(get == 1){ //H3lis331.Get
+        int16_t rx[3];
+        H3lis331.Get(rx); //3軸の加速度を取得
+        Serial.print(rx[0]); //取得した各軸の加速度を表示
+        Serial.print(",");
+        Serial.print(rx[1]);
+        Serial.print(",");
+        Serial.println(rx[2]);
+        delay(1000);
+    }
 
-    // }
+    if(get == 2){ //H3lis331.Get2
+        int16_t rx[3];
+        uint8_t rx_buf[6];
+        H3lis331.Get2(rx , rx_buf); //3軸の加速度を取得
+        Serial.print(rx[0]); //取得した各軸の加速度を表示
+        Serial.print(",");
+        Serial.print(rx[1]);
+        Serial.print(",");
+        Serial.println(rx[2]);
+        delay(1000);
+    }
+}
 }
