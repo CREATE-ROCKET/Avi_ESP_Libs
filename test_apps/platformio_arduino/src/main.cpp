c97d0f47d0a59689dc24f0007f9fa993e5ede43e
Natsu-B <natsu.minatomirai@gmail.com>
2026-08-05 06:29:16 +0900

    test: add PlatformIO build smoke tests

---
+++ test_apps/platformio_arduino/src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+#include <CANCREATE.h>
+#include <H3LIS331.h>
+#include <ICM20602.h>
+#include <ICM20948.h>
+#include <ICM42688.h>
+#include <LPS25HB.h>
+#include <NEC920.h>
+#include <S25FL127S.h>
+#include <S25FL512S.h>
+#include <SPICREATE.h>
+void setup(){SPICREATE spi;CANCREATE can;ICM42688 a;ICM20948 b;ICM20602 c;H3LIS331 d;LPS25HB e;S25FL127S f;S25FL512S g;NEC920 h;(void)spi;(void)can;(void)a;(void)b;(void)c;(void)d;(void)e;(void)f;(void)g;(void)h;}
+void loop(){}
